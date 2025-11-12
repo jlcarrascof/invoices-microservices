11afbe9ef67a4e3f4c5c761269b6684e12a3d786
Javier J. Martinez F <entrenamientospuntonet@gmail.com>
2025-11-12 18:58:59 -0400

    feat: add UpdateInvoiceItemRequest for invoice item data validation

---
+++ backend/invoices-laravel/app/Http/Requests/UpdateInvoiceItemRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateInvoiceItemRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        return [
+            'description' => 'required|string|max:255|min:3',
+            'quantity' => 'required|integer|min:1',
+            'unit_price' => 'required|numeric|min:0.01',
+        ];
+    }
+
+    /**
+     * Get custom error messages for validation rules.
+     */
+    public function messages(): array
+    {
+        return [
+            'description.required' => 'The description of the item is required',
+            'description.min' => 'The description must be at least 3 characters',
+            'quantity.required' => 'The quantity is required',
+            'quantity.min' => 'The quantity must be greater than 0',
+            'unit_price.required' => 'The unit price is required',
+            'unit_price.min' => 'The unit price must be greater than 0',
+        ];
+    }
+}
